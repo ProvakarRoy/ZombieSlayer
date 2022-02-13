088f6f2957c8633d1877b33fad2bca7f9f4d01e3
ProvakarRoy <pr.provakar@gmail.com>
2022-02-14 01:03:24 +0530

    Zombie Ai using raycaste

--- Assets/_ZombieSlayer_/Scripts/Gameplay/AI_Controller.cs
+++ Assets/_ZombieSlayer_/Scripts/Gameplay/AI_Controller.cs
@@ -12,10 +12,19 @@ public class AI_Controller : MonoBehaviour
     [SerializeField]private float FieldOfView = 120f;
     [SerializeField]private float ViewDistance = 10f;
     private Animator animator;
+    private AudioSource audioSource;
+
+    public AudioClip ShootSound;
+    [SerializeField]private float SoundIntensity=5f;
+    public LayerMask ZombieLayers;
+
+    public object Zombies { get; private set; }
+
     private void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
         animator = this.GetComponent<Animator>();
+        audioSource = this.GetComponent<AudioSource>();
     }
     void Update()
     {
@@ -36,7 +45,14 @@ public class AI_Controller : MonoBehaviour
         {
             if (Vector3.Distance(Player.transform.position, this.transform.position) < ViewDistance)
             {
-                OnAware();
+                RaycastHit hit;
+                if(Physics.Linecast(this.transform.position,Player.transform.position,out hit, -1))
+                {
+                    if (hit.transform.tag == "Player")
+                    {
+                        OnAware();
+                    }
+                }
             }
         }
     }
@@ -44,6 +60,17 @@ public class AI_Controller : MonoBehaviour
     public void OnAware()
     {
         isAware = true;
+    }
+
+    public void Onfire()
+    {
+        AI_Controller callingOnAware = new AI_Controller();
+        audioSource.PlayOneShot(ShootSound);
+        Collider[] Zombies = Physics.OverlapSphere(transform.position, SoundIntensity, ZombieLayers);
+        for(int i = 0; i < Zombies.Length; i++)
+        {
+            Zombies[i].GetComponent<AI_Controller>().OnAware(); 
+        }
     }
 
     public void OnTriggerEnter(Collider other)
